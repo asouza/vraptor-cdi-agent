d5852d9e8f7614773a621f629f8c6b17d6558505
Alberto Souza <alots.ssa@gmail.com>
2013-01-17 10:58:01 -0200

    first version of vraptor cdi agent

---
+++ src/br/com/caelum/vraptor/cdi/agent/CDIClassTransformer.java
@@ -0,0 +1,35 @@
+package br.com.caelum.vraptor.cdi.agent;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+import javassist.ByteArrayClassPath;
+import javassist.ClassPool;
+import javassist.CtClass;
+
+public class CDIClassTransformer implements ClassFileTransformer {
+	
+	private ClassPool pool = ClassPool.getDefault();
+
+	public byte[] transform(ClassLoader loader, String className,
+			Class<?> classBeingRedefined, ProtectionDomain protectionDomain,
+			byte[] classfileBuffer) throws IllegalClassFormatException {		
+		try {
+			className = className.replace('/', '.');
+			pool.insertClassPath(new ByteArrayClassPath(className, classfileBuffer));
+			CtClass modified = new ConstructorAdapter(pool)
+			.tryToAddCDIConstructorFor(className);			
+			return modified.toBytecode();
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	public static void premain(String agentArgs, Instrumentation inst) {
+		inst.addTransformer(new CDIClassTransformer());
+
+	}
+
+}
---
+++ src/br/com/caelum/vraptor/cdi/agent/ConstructorAdapter.java
@@ -0,0 +1,122 @@
+package br.com.caelum.vraptor.cdi.agent;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.NotFoundException;
+import javassist.bytecode.AnnotationsAttribute;
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.ConstPool;
+import javassist.bytecode.annotation.Annotation;
+import br.com.caelum.vraptor.Convert;
+import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.VRaptorException;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+import br.com.caelum.vraptor.ioc.Component;
+import br.com.caelum.vraptor.ioc.PrototypeScoped;
+import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.ioc.SessionScoped;
+
+//TODO unit tests
+public class ConstructorAdapter {
+	private static Map<String, CtClass> adaptedClasses = new HashMap<String, CtClass>();
+	private final ClassPool classPool;
+
+	public ConstructorAdapter(ClassPool classPool) {
+		this.classPool = classPool;
+
+	}
+
+	public CtClass tryToAddCDIConstructorFor(String className) {
+		if (adaptedClasses.containsKey(className)) {
+			return adaptedClasses.get(className);
+		}
+		try {
+			CtClass ctClass = classPool.get(className);
+			if (isAValidClass(ctClass) && !thereIsNoArgsConstructor(ctClass)) {
+				CtConstructor defaultConstructor = new CtConstructor(
+						new CtClass[] {}, ctClass);
+				defaultConstructor.setBody("{}");
+				addInjectionAnnotationsForAllProviders(ctClass);
+				ctClass.addConstructor(defaultConstructor);
+			}
+			return ctClass;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@SuppressWarnings("unchecked")
+	private void addInjectionAnnotationsForAllProviders(CtClass ctClass)
+			throws NotFoundException {
+		CtConstructor constructor = getFirstConstructorWithArgs(ctClass);
+		if (constructor != null) {
+			//for some reasone, javassist needs both annotation. If i add one
+			//by one, it can't keep all.
+			add(ctClass, constructor,Arrays.asList(Inject.class,Autowired.class));
+		}
+	}
+
+	private void add(CtClass ctClass, CtConstructor constructor,
+			List<Class<? extends java.lang.annotation.Annotation>> annotationsToBeAdded) {
+			ClassFile classFile = ctClass.getClassFile();
+			ConstPool constPool = classFile.getConstPool();
+			AnnotationsAttribute annotationsContext = new AnnotationsAttribute(
+					constPool, AnnotationsAttribute.visibleTag);
+			ArrayList<Annotation> annotations = new ArrayList<Annotation>(
+					Arrays.asList(annotationsContext.getAnnotations()));
+			for (Class<? extends java.lang.annotation.Annotation> annotation : annotationsToBeAdded) {
+				Annotation injectAnnotation = new Annotation(
+						annotation.getCanonicalName(), constPool);
+				annotations.add(injectAnnotation);				
+			}
+			annotationsContext.setAnnotations(annotations
+					.toArray(new Annotation[] {}));
+			constructor.getMethodInfo().addAttribute(annotationsContext);
+	}
+
+	private CtConstructor getFirstConstructorWithArgs(CtClass ctClass)
+			throws NotFoundException {
+		CtConstructor[] constructors = ctClass.getDeclaredConstructors();
+		for (CtConstructor ctConstructor : constructors) {
+			if (ctConstructor.getParameterTypes().length > 0) {
+				return ctConstructor;
+			}
+		}
+		return null;
+	}
+
+	private boolean isAValidClass(CtClass ctClass) {
+		return !ctClass.isAnnotation() && !ctClass.isInterface() && !ctClass.isEnum() && !ctClass.isArray()
+				&& (ctClass.hasAnnotation(Component.class)
+						|| ctClass.hasAnnotation(Resource.class)
+						|| ctClass.hasAnnotation(ApplicationScoped.class)
+						|| ctClass.hasAnnotation(SessionScoped.class)
+						|| ctClass.hasAnnotation(RequestScoped.class)
+						|| ctClass.hasAnnotation(PrototypeScoped.class) 
+						|| ctClass.hasAnnotation(Convert.class));
+	}
+
+	private boolean thereIsNoArgsConstructor(CtClass ctClass)
+			throws NotFoundException {
+		CtConstructor[] constructors = ctClass.getDeclaredConstructors();
+		for (CtConstructor ctConstructor : constructors) {
+			if (ctConstructor.getParameterTypes().length == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
